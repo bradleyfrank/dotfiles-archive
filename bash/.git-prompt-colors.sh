65d9ed8dba3e32c405337c6688d81110fc085149
Brad <bradley.frank@gmail.com>
2018-09-05 10:24:30 -0400

    Added new prompt character to PS1.

--- bash/.git-prompt-colors.sh
+++ bash/.git-prompt-colors.sh
@@ -13,12 +13,12 @@ override_git_prompt_colors() {
   GIT_PROMPT_CLEAN="${Green}✓"
   GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="✭"
 
-  GIT_PROMPT_COMMAND_OK="${ResetColor}"
-  GIT_PROMPT_COMMAND_FAIL="${Red}($?)${ResetColor}"
+  GIT_PROMPT_COMMAND_OK="${White}⋊>${ResetColor} "
+  GIT_PROMPT_COMMAND_FAIL="${Red}⋊>${ResetColor} "
 
-  GIT_PROMPT_START_USER="${ResetColor}${Yellow}${PathShort}${ResetColor}"
-	GIT_PROMPT_END_USER="_LAST_COMMAND_INDICATOR_${Green} ${ResetColor}"
-	GIT_PROMPT_END_ROOT="_LAST_COMMAND_INDICATOR_${Green} ${ResetColor}"
+  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_${Yellow}${PathShort}${ResetColor}"
+	GIT_PROMPT_END_USER="${ResetColor} "
+	GIT_PROMPT_END_ROOT="${ResetColor} "
 }
 
 reload_git_prompt_colors "Custom_1"
