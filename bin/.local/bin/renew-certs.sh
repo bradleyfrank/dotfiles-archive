b62fd3f40f54a1ba98bc2c7d005ccf8652dee52b
Brad <bradley.frank@gmail.com>
2018-11-16 12:10:26 -0500

    Run script through shellcheck.

--- bin/.local/bin/renew-certs.sh
+++ bin/.local/bin/renew-certs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-today="$(date +%Y-%m-%d)"
-tmpdir="$(mktemp -d)"
+set -eu
 
+#
+# Meant to be run on the Synology only.
+#
+
+today="$(date +%Y-%m-%d)"
 archive_dir="/root/certs/$today"
 mkdir -p "$archive_dir" >/dev/null
 
@@ -12,35 +16,31 @@ if [[ ! -f /root/certs/fullchain.pem ]] || [[ ! -f /root/certs/privkey.pem ]]; t
 fi
 
 pushd /usr/syno/etc/certificate/_archive/ARKpqt >/dev/null
-tar -czf "$tmpdir"/ARKpqt_"$today".tar.gz *.pem
+tar -czf "$archive_dir"/ARKpqt_"$today".tar.gz ./*.pem
 cp /root/certs/*.pem .
 popd >/dev/null
 
 pushd /usr/syno/etc/certificate/smbftpd/ftpd >/dev/null
-tar -czf "$tmpdir"/ftpd_"$today".tar.gz *.pem
+tar -czf "$archive_dir"/ftpd_"$today".tar.gz ./*.pem
 cp /root/certs/*.pem .
 popd >/dev/null
 
 pushd /usr/local/etc/certificate/LogCenter/pkg-LogCenter >/dev/null
-tar -czf "$tmpdir"/pkg-LogCenter_"$today".tar.gz *.pem
+tar -czf "$archive_dir"/pkg-LogCenter_"$today".tar.gz ./*.pem
 cp /root/certs/*.pem .
 popd >/dev/null
 
 pushd /usr/local/etc/certificate/CloudStation/CloudStationServer >/dev/null
-tar -czf "$tmpdir"/CloudStationServer_"$today".tar.gz *.pem
+tar -czf "$archive_dir"/CloudStationServer_"$today".tar.gz ./*.pem
 cp /root/certs/*.pem .
 popd >/dev/null
 
 pushd /usr/local/etc/certificate/CardDAVServer/carddav >/dev/null
-tar -czf "$tmpdir"/carddav_"$today".tar.gz *.pem
+tar -czf "$archive_dir"/carddav_"$today".tar.gz ./*.pem
 cp /root/certs/*.pem .
 popd >/dev/null
 
 pushd /usr/local/etc/certificate/VPNCenter/OpenVPN >/dev/null
-tar -czf "$tmpdir"/OpenVPN_"$today".tar.gz *.pem
+tar -czf "$archive_dir"/OpenVPN_"$today".tar.gz ./*.pem
 cp /root/certs/*.pem .
-popd >/dev/null
-
-pushd "$tmpdir" >/dev/null
-mv *.tar.gz "$archive_dir"/
 popd >/dev/null
